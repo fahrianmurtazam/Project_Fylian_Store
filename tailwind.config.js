52403268ec8b77e9df71ab4373213a0ed5b9b757
fahrianmurtazam <fahrian22@mhs.usk.ac.id>
2024-09-18 11:24:16 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#b6895b",
+        bg: "#010101",
+      },
+      container:{
+        center:true,
+      }
+    },
+  },
+  plugins: [],
+};
